2a61e24fb3df5d33ebbc1acb0e1e251bd4d50ea6
Dhrumal-Patel <dhrumal832@gmail.com>
2022-06-27 22:21:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'Authenticate/LoginScreen.dart';
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: FirebaseOptions(apiKey: "XXX", appId: "XXX", messagingSenderId: "XXX", projectId: "XXX"),
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
